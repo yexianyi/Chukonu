5d6f1d2caa6d8b5ced5a042c1b88bf18f52c2e5d
yexianyi <xianyiye@XIANYIYE-WS.cisco.com>
2015-07-27 16:41:20 +0800

    add extract weblink utility

---
+++ Extract_weblink_with_Reg/Extract_weblink_with_Reg.java
@@ -0,0 +1,8 @@
+String reg = "((http|https|ftp)\\://)?([a-zA-Z0-9\\.\\-]+(\\:[a-zA-Z0-9\\.&amp;%\\$\\-]+)*@)?((25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9])\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[1-9]|0)\\.(25[0-5]|2[0-4][0-9]|[0-1]{1}[0-9]{2}|[1-9]{1}[0-9]{1}|[0-9])|([a-zA-Z0-9\\-]+\\.)*[a-zA-Z0-9\\-]+\\.[a-zA-Z]{2,4})(\\:[0-9]+)?(/[^/][a-zA-Z0-9\\.\\,\\?\\'\\\\/\\+&amp;%\\$#\\=~_\\-@]*)*";
+Pattern pattern = Pattern.compile(reg);
+Matcher matcher = pattern.matcher("my link is http://www.sohu.com afadfadf adfasd adfa, https://www.google.com,adfadf, www.google.com");
+StringBuffer buffer = new StringBuffer();
+while(matcher.find()){              
+    System.out.println(matcher.group());
+}   
+
