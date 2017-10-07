9ba4bb503e2ae18aa72fdae77deaf287431b4e6e
Xianyi Ye <yexianyi@hotmail.com>
2017-10-07 17:10:26 +0800

    update dynmaic proxy example

--- chukonu_java/src/main/java/com/yxy/chukonu/java/dynamic/proxy/DynamicProxyWithJDK.java
+++ chukonu_java/src/main/java/com/yxy/chukonu/java/dynamic/proxy/DynamicProxyWithJDK.java
@@ -52,12 +52,6 @@ public class DynamicProxyWithJDK implements InvocationHandler{
         Human man = new Man();
         //inject delegation instance to handler
         InvocationHandler handler = new DynamicProxyWithJDK(man);
-        /*
-         * 通过Proxy的newProxyInstance方法来创建我们的代理对象，我们来看看其三个参数
-         * 第一个参数 handler.getClass().getClassLoader() ，我们这里使用handler这个类的ClassLoader对象来加载我们的代理对象
-         * 第二个参数realSubject.getClass().getInterfaces()，我们这里为代理对象提供的接口是真实对象所实行的接口，表示我要代理的是该真实对象，这样我就能调用这组接口中的方法了
-         * 第三个参数handler， 我们这里将这个代理对象关联到了上方的 InvocationHandler 这个对象上
-         */
         Human proxyInstance = (Human)Proxy.newProxyInstance(handler.getClass().getClassLoader(), man.getClass().getInterfaces(), handler);
         proxyInstance.getName() ;
     }
