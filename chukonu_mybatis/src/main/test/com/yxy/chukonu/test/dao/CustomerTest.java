452d3e8b2355e523832db966dc733b7c77ec5d6c
Xianyi Ye <yexianyi@hotmail.com>
2017-10-27 10:29:57 +0800

    add Junit4 test cases

--- chukonu_mybatis/src/main/test/com/yxy/chukonu/test/dao/CustomerTest.java
+++ chukonu_mybatis/src/main/test/com/yxy/chukonu/test/dao/CustomerTest.java
@@ -1,5 +1,7 @@
 package com.yxy.chukonu.test.dao;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -7,12 +9,19 @@ import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
 import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
 
 import com.yxy.chukonu.mybatis.mapper.Customer;
+import com.yxy.chukonu.mybatis.mapper.CustomerMapper;
 
 public class CustomerTest {
 	
-	public static void main(String[] args){
+	private SqlSession session ;
+	
+	@Before
+	public void createSession(){
 		String resource = "mybatis-config.xml";
 		InputStream inputStream = null;
 		try {
@@ -21,13 +30,31 @@ public class CustomerTest {
 			e.printStackTrace();
 		}
 		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		SqlSession session = sqlSessionFactory.openSession();
-		try {
-			Customer customer = (Customer) session.selectOne("findCustomer", "2");
-			System.out.println(customer.getName());
-		} finally {
-		  session.close();
+		session = sqlSessionFactory.openSession();
+	}
+	
+	
+	@Test
+	public void testQueryBySqlSession() {
+		Customer result = (Customer) session.selectOne("findCustomer", "2");
+		assertEquals("aaa", result.getName());
+	}
+	
+	
+	@Test
+	public void testQueryByMapper() {
+		CustomerMapper mapper = session.getMapper(CustomerMapper.class) ;
+		Customer result = mapper.findCustomer("2") ;
+		assertEquals("aaa", result.getName());
+	}
+	
+	
+	@After
+	public void closeSession(){
+		if(session != null){
+			session.close();
 		}
 	}
+	
 
 }
