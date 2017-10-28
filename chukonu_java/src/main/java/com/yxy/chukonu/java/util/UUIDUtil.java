e7be0c3804e1940b44b038bc15b6b782e9d684e1
Xianyi Ye <yexianyi@hotmail.com>
2017-10-28 21:19:36 +0800

    add util to generate uuid

---
+++ chukonu_java/src/main/java/com/yxy/chukonu/java/util/UUIDUtil.java
@@ -0,0 +1,16 @@
+package com.yxy.chukonu.java.util;
+
+import java.util.UUID;
+
+public class UUIDUtil {
+
+	public static void main(String[] args) {
+        
+		UUID uuid = UUID.randomUUID() ;
+		         
+		System.out.println(uuid);
+		     
+		}
+
+
+}
