ee2a2bdca345d457fa1d4e4f46c9cec0eb1082e8
Xianyi Ye <yexianyi@hotmail.com>
2017-10-29 23:04:38 +0800

    add spring-batis config and test case

--- chukonu_spring_mybatis/src/test/java/com/yxy/chukonu/test/dao/AccountTest.java
+++ chukonu_spring_mybatis/src/test/java/com/yxy/chukonu/test/dao/AccountTest.java
@@ -12,6 +12,9 @@ import org.apache.ibatis.session.SqlSessionFactoryBuilder;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.yxy.chukonu.mybatis.mapper.account.Account;
 import com.yxy.chukonu.mybatis.mapper.account.AccountMapper;
@@ -20,42 +23,22 @@ import com.yxy.chukonu.mybatis.mapper.customer.CustomerMapper;
 
 public class AccountTest {
 	
-	private SqlSession session ;
+	private SqlSessionTemplate sqlSessionTemplate ;
 	
 	@Before
 	public void createSession(){
-		String resource = "mybatis-config.xml";
-		InputStream inputStream = null;
-		try {
-			inputStream = Resources.getResourceAsStream(resource);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
-		session = sqlSessionFactory.openSession();
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-mybatis.xml") ;
+		sqlSessionTemplate = ctx.getBean(SqlSessionTemplate.class) ;
 	}
 	
 	
 	@Test
 	public void testQueryBySqlSession() {
-		Account result = (Account) session.selectOne("findAccount", "aa889216-2dec-437b-87b0-1272d12a80c8");
+		Account account = new Account() ;
+		account.setId("aa889216-2dec-437b-87b0-1272d12a80c8");
+		
+		Account result = (Account) sqlSessionTemplate.selectOne("com.yxy.chukonu.mybatis.mapper.account.AccountMapper.findAccount",account);
 		assertEquals("1000100010001000", result.getAcc_number());
-	}
-	
-	
-	@Test
-	public void testQueryByMapper() {
-		AccountMapper mapper = session.getMapper(AccountMapper.class) ;
-		Account result = mapper.findAccount("89ab5a34-0e07-43e2-a5e6-8b0ad8446a26") ;
-		assertEquals("2000200020002000", result.getAcc_number());
-	}
-	
-	
-	@After
-	public void closeSession(){
-		if(session != null){
-			session.close();
-		}
 	}
 	
 
---
+++ chukonu_spring_mybatis/src/test/java/com/yxy/chukonu/test/dao/CustomerTest.java
@@ -0,0 +1,34 @@
+package com.yxy.chukonu.test.dao;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import com.yxy.chukonu.mybatis.mapper.customer.Customer;
+
+public class CustomerTest {
+	
+	private SqlSessionTemplate sqlSessionTemplate ;
+	
+	@Before
+	public void createSessionTemplate(){
+		ApplicationContext ctx = new ClassPathXmlApplicationContext("spring-mybatis.xml") ;
+		sqlSessionTemplate = ctx.getBean(SqlSessionTemplate.class) ;
+	}
+	
+	
+	@Test
+	public void testQueryBySqlSessionTemplate() {
+		Customer customer = new Customer() ;
+		customer.setId("652300f0-2a8b-4728-8704-3968a8819a5a");
+		
+		Customer result = (Customer) sqlSessionTemplate.selectOne("com.yxy.chukonu.mybatis.mapper.customer.CustomerMapper.findCustomer",customer);
+		assertEquals("aaa", result.getName());
+	}
+	
+	
+}
