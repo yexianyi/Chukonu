2a6b56caa241b0b188d85fbc5ed11c60b88a416b
Xianyi Ye <yexianyi@hotmail.com>
2017-12-05 15:39:07 +0800

    add JDK 1.8 new feature - predicate interface

---
+++ chukonu_java/src/main/java/com/yxy/chukonu/java/jdk18/func/predicate/Example.java
@@ -0,0 +1,21 @@
+package com.yxy.chukonu.java.jdk18.func.predicate;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class Example {
+
+	public static void main(String[] args) {
+		Predicate<String> predicate = (s) -> s.length() > 0;
+		predicate.test("foo");              // true
+		predicate.negate().test("foo");     // false
+
+		Predicate<Boolean> nonNull = Objects::nonNull;
+		Predicate<Boolean> isNull = Objects::isNull;
+
+		Predicate<String> isEmpty = String::isEmpty;
+		Predicate<String> isNotEmpty = isEmpty.negate();
+
+	}
+
+}
