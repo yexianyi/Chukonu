b5866da07f543111be1111d47d704d64d2c22587
Xianyi Ye <yexianyi@hotmail.com>
2017-12-15 22:28:28 +0800

    enhance swagger and refine testing service

--- chukonu_spring_boot/src/main/java/org/chukonu/spring/boot/ChukonuSpringBootApplication.java
+++ chukonu_spring_boot/src/main/java/org/chukonu/spring/boot/ChukonuSpringBootApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 @EnableAutoConfiguration
+@EnableSwagger2
 @ComponentScan(basePackageClasses = FunctionController.class)
 public class ChukonuSpringBootApplication {
 
--- chukonu_spring_boot/src/main/java/org/chukonu/spring/boot/controller/FunctionController.java
+++ chukonu_spring_boot/src/main/java/org/chukonu/spring/boot/controller/FunctionController.java
@@ -1,16 +1,17 @@
 package org.chukonu.spring.boot.controller;
 
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
-@RequestMapping("/func")
+@RequestMapping("/function")
 public class FunctionController {
 
-    @RequestMapping("/mvc1")
-    public ModelAndView mvc1() {
+    @GetMapping("/test1")
+    public ModelAndView test1() {
         return new ModelAndView("index");
     }
 }
