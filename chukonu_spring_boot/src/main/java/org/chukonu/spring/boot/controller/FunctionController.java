c43c1c1ccda73fcaf9ff72f45c04c17ed42a5a5c
Xianyi Ye <yexianyi@hotmail.com>
2017-12-15 17:54:24 +0800

    update pom

--- chukonu_spring_boot/src/main/java/org/chukonu/spring/boot/controller/FunctionController.java
+++ chukonu_spring_boot/src/main/java/org/chukonu/spring/boot/controller/FunctionController.java
@@ -1,6 +1,8 @@
 package org.chukonu.spring.boot.controller;
 
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 @RestController
