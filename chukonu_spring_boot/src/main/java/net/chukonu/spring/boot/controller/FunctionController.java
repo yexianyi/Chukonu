7bc286d990c63fb6c9b8290bd9156a8fa112c087
yexianyi <yexianyi@baidu.com>
2018-07-27 17:16:08 +0800

    Enhance capability of build and push docker image

--- chukonu_spring_boot/src/main/java/net/chukonu/spring/boot/controller/FunctionController.java
+++ chukonu_spring_boot/src/main/java/net/chukonu/spring/boot/controller/FunctionController.java
@@ -1,11 +1,17 @@
 package net.chukonu.spring.boot.controller;
 
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
+
+import net.chukonu.spring.boot.param.AddressResult;
 
 @RestController
 @RequestMapping("/function")
@@ -19,6 +25,20 @@ public class FunctionController {
     @GetMapping("/test2")
     public ResponseEntity<String>  test2() {
     	return ResponseEntity.ok("OK");
+    }
+    
+    @GetMapping("/local_ip")
+    @ResponseBody
+    public AddressResult getLocalIPAddr() {
+    	InetAddress address;
+		try {
+			address = InetAddress.getLocalHost();
+			return new AddressResult(address.getHostAddress()) ;     
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} 
+		
+		return null;
     }
 }
 
---
+++ chukonu_spring_boot/src/main/java/net/chukonu/spring/boot/param/AddressResult.java
@@ -0,0 +1,12 @@
+package net.chukonu.spring.boot.param;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class AddressResult {
+	
+	String address ;
+
+}
