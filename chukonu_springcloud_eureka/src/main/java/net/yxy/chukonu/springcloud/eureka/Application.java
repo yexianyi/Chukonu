bc2d931b815235ea437a4445f10e097e1659bb49
yexianyi <yexianyi@baidu.com>
2018-08-23 14:10:46 +0800

    add rest api

--- chukonu_springcloud_eureka/src/main/java/net/yxy/chukonu/springcloud/eureka/Application.java
+++ chukonu_springcloud_eureka/src/main/java/net/yxy/chukonu/springcloud/eureka/Application.java
@@ -3,20 +3,20 @@ package net.yxy.chukonu.springcloud.eureka;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @EnableEurekaServer
 @EnableEurekaClient
 @SpringBootApplication
-//@RestController
+@RestController
 public class Application {
     
-//    @RequestMapping("/")
-//    public String home() {
-//        return "Hello world";
-//    }
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world";
+    }
 
     
     public static void main(String[] args) {
